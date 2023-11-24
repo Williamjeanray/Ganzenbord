cb8501e65d64c8b9697b21cb8d5e78df2100a326
William <wljr@fwo.be>
2023-11-24 12:57:44 +0100

    Einde Les

---
+++ Ganzenbord.Business.Tests/SkipTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ganzenbord.Business.Tests
+{
+    internal class SkipTests
+    {
+        [Test]
+        public void WhenPlayerHasToSkipTurn_ThenTheyCannotMove()
+        {
+            //Arrange
+            IPlayer player = new Player();
+            player.Position = 16;
+            player.DiceRoll = 3;
+
+            //Act
+            player.Move(player.DiceRoll);
+
+            //Assert
+            Assert.That(player.Position, Is.EqualTo(19));
+            Assert.That(player.TurnsToSkip, Is.EqualTo(1));
+
+            player.Move(player.DiceRoll);
+            Assert.That(player.Position, Is.EqualTo(19));
+            Assert.That(player.TurnsToSkip, Is.EqualTo(0));
+
+            player.Move(player.DiceRoll);
+            Assert.That(player.Position, Is.EqualTo(22));
+            Assert.That(player.TurnsToSkip, Is.EqualTo(0));
+
+        }
+    }
+}
